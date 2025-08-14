6535ee9e6bbecc97c1e2e6af1b684574256a02b9
Ini Arthur <arthurshur@gmail.com>
2025-08-14 22:50:44 +0000

    cart

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from .models import Product, Cart
 from flask_login import login_required, current_user
-from flask import Blueprint, render_template, flash, redirect
+from flask import Blueprint, render_template, flash, redirect, request
+from . import db
 
 views = Blueprint('views', __name__)
 
@@ -8,7 +9,8 @@ views = Blueprint('views', __name__)
 def home():
     items = Product.query.filter_by(flash_sale=True)
 
-    return render_template("home.html", items = items) 
+    return render_template("home.html", items = items, cart = Cart.query.filter_by(customer_link=current_user.id).all()
+                           if current_user.is_authenticated else []) 
 
 
 
@@ -16,7 +18,40 @@ def home():
 @login_required
 def add_to_cart(item_id):
     item_to_add = Product.query.get(item_id)
-    item_exists = Cart.query.filter_by(product_link=item_id, customer_link=current_user).first()
+    item_exists = Cart.query.filter_by(product_link=item_id, customer_link=current_user.id).first()
     if item_exists:
         try:
-            item_exists.quantity = item_exists.quantity + 1
+            item_exists.quantity += item_exists.quantity + 1
+            db.session.commit()
+            flash(f'Quantity of {item_to_add.product_name} updated.')
+            return redirect(request.referrer)
+        except Exception as e:
+            print( 'Quantity not updated', e)
+            flash(f'Quantity of {item_to_add.product_name} not updated.')
+            return redirect(request.referrer)
+        
+    new_cart_item = Cart()
+    new_cart_item.quantity = 1
+    new_cart_item.product_link = item_to_add.id
+    new_cart_item.customer_link = current_user.id
+
+    try:
+        db.session.add(new_cart_item)
+        db.session.commit()
+        flash(f'{new_cart_item.product_name} added to cart.')
+    except Exception as e:
+        print('Item not added to cart', e)
+        flash(f'{new_cart_item.product.product_name} not added to cart.')
+
+    return redirect(request.referrer)
+
+
+@views.route('/cart')
+@login_required
+def show_cart():
+    cart = Cart.query.filter_by(customer_link=current_user.id).all()
+    amount = 0
+    for item in cart:
+        amount += item.product.current_price  * item.quantity
+
+    return render_template('cart.html', cart=cart, amount=amount, total=amount+200)
